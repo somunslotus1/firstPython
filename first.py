10c9fe70824bb7767fde4aa0ba47ce211e8e14ff
clj <somunslotus@163.com>
2016-08-14 23:12:09 +0800

    add while usage

--- first.py
+++ first.py
@@ -8,3 +8,9 @@ for item in numberList:
 myname = raw_input('what is your name:')
 print myname
 
+index = 0
+while index < 1000:
+    print index,
+    if index % 10 == 0:
+        print("\n")
+    index = index + 1
